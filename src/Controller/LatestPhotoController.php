9d4aaecb4ae27b96468901de8cc6b8c8e6bbdb88
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2024-08-08 23:25:20 +0200

    08-08-2024 latest photos

--- src/Controller/LatestPhotoController.php
+++ src/Controller/LatestPhotoController.php
@@ -1,14 +1,29 @@
 <?php
-
 namespace App\Controller;
 
-class LatestPhotoController
+use App\Entity\Photo;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LatestPhotoController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index()
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('latest_photo/index.html.twig', [
-            'controller_name' => 'LatestPhotoController',
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/latest', name: 'latest_photos')]
+    public function index(): Response
+    {
+        $latestPhotosPublic = $this->entityManager->getRepository(Photo::class)
+            ->findBy(['is_public' => true]);
+
+        return $this->render('latest_photos/index.html.twig', [
+            'latestPhotosPublic' => $latestPhotosPublic,
         ]);
     }
 }
